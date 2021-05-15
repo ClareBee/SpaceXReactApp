7b88eac8ff0cf53e77b1819b4e278a4972a94fb1
Clare <chblackburne@gmail.com>
2021-05-15 13:56:39 +0100

    Add styled components and global reset

--- spacex/src/App.tsx
+++ spacex/src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <div>Hello world</div>;
-}
+import styled from 'styled-components';
+import { GlobalStyle } from './styles/GlobalStyle';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: blue;
+`;
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <Title>Hello world</Title>
+  </>
+);
 
 export default App;
