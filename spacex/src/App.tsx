03a916686f634ba5205ff0f9f297beb8d1f0660c
Clare <chblackburne@gmail.com>
2021-05-17 12:46:33 +0100

    Better error handling

--- spacex/src/App.tsx
+++ spacex/src/App.tsx
@@ -1,17 +1,20 @@
 import { FunctionComponent, useState, useEffect } from 'react';
 
-import { Loader } from './components/Loader/Loader';
-import { useGetData } from './services/api/useGetData';
-import { GlobalStyle } from './styles/GlobalStyle';
-import List from './components/List/List';
 import Layout from './containers/Layout';
-import RocketImage from './components/RocketImage/RocketImage';
-import { Header } from './components/Header/Header';
+import ErrorBoundary from './containers/ErrorBoundary';
+import ListContainer from './containers/ListContainer';
+
+import { useGetData } from './services/api/useGetData';
 import { filterByYear } from './utils/filterByYear';
 import { sortByYear } from './utils/sortByYear';
-import ListContainer from './containers/ListContainer';
-import Actions from './components/Actions/Actions';
 import { Launch } from './types/types';
+import { GlobalStyle } from './styles/GlobalStyle';
+
+import List from './components/List/List';
+import { Loader } from './components/Loader/Loader';
+import RocketImage from './components/RocketImage/RocketImage';
+import { Header } from './components/Header/Header';
+import Actions from './components/Actions/Actions';
 
 const App: FunctionComponent = () => {
   const [data, setData] = useState<Launch[] | []>([]);
@@ -48,7 +51,7 @@ const App: FunctionComponent = () => {
   };
 
   return (
-    <>
+    <ErrorBoundary>
       <GlobalStyle />
       <Header handleReload={handleReload} reload={reload} />
       <Layout>
@@ -64,7 +67,7 @@ const App: FunctionComponent = () => {
           {loading ? <Loader /> : <List listItems={data} />}
         </ListContainer>
       </Layout>
-    </>
+    </ErrorBoundary>
   );
 };
 
